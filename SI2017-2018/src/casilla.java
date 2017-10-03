8a33e571da3cec19bb7632531ee6fce35912564e
Carlos <Carlos@172.19.178.16>
2017-10-03 17:09:58 +0200

    xd

---
+++ SI2017-2018/src/casilla.java
@@ -0,0 +1,17 @@
+
+public class casilla {
+	int cantidad=0;
+	int fila=0;
+	int columna=0;
+	public casilla(int cantidad,int fila,int columna)
+	{
+		this.cantidad=cantidad;
+		this.fila=fila;
+		this.columna=columna;
+	}
+	
+	public int getCantidad()
+	{
+		return this.cantidad;
+	}
+}
